a0609a6fba56d7cb7a40b9d46d501b59d6984370
Tim Kanarsky <tkanarsky@outlook.com>
2022-05-09 22:11:57 -0700

    initial

---
+++ build.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+from json import load
+
+
+DATAFILE_NAME = "mime_data.json"
+STATIC_DIR = "static/"
+
+
+if __name__ == "__main__":
+    with open(DATAFILE_NAME, 'r') as f:
+        d = load(f)
+    for item in d:
+        mime_head, mime_tail = item["mime"].split("/")
+        ext = item["ext"]
+        Path(STATIC_DIR + mime_head + "/").mkdir(exist_ok=True)
+        with open(STATIC_DIR + mime_head + "/" + mime_tail, 'w') as f:
+            f.write(ext)
